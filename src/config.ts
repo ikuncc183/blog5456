97283fb28e998a8904b4f10edaa7c1a42c3dd473
ikun <1726180352@qq.com>
2025-07-09 13:45:09 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -33,7 +33,7 @@ export default {
     // 其他页面高度
     PageHeight: '28.88rem',
     // 背景
-    background: "url('/assets/images/雪花飞舞_爱给网_aigei_com.gif') no-repeat center 60%/cover",
+    background: "url('/assets/images/bbad4c945b480d3b24302642f341a87e.mp4') no-repeat center 60%/cover",
   },
   // 博客主题配置
   Theme: {
